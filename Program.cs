4124f71aae3234e1151e6e306d749323a7942b9c
Homak-Rumata <108110082+Homak-Rumata@users.noreply.github.com>
2022-06-24 10:23:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Код_по_схемам__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 x, p;
+            Double v;
+            Console.Write("х = ");
+            x = Int32.Parse(Console.ReadLine());
+            Console.Write("p = ");
+            p = Int32.Parse(Console.ReadLine());
+            v = x * p * 0.001;
+            Console.WriteLine($"v = {v}");
+            Console.ReadLine();
+        }
+    }
+}
